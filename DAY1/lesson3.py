c18e6f08c986f71c98f65617b4297e91dded9951
Leshan21 <leshanpasindu12@gmail.com>
2024-10-25 21:37:58 +0530

    Q3

---
+++ DAY1/lesson3.py
@@ -0,0 +1,4 @@
+print("Day 1 - String Manipulation")
+print("String Concatenation is done withh the "+"sign.")
+print('e.g. print("Hello "+ "world")')
+print(("New lines can created with a hacklash and n."))

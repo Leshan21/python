06219e738cf3f5f3ed021883712947f82f8950ef
Leshan21 <leshanpasindu12@gmail.com>
2024-10-26 19:32:09 +0530

    exercise1

---
+++ DAY2/Exercise1.py
@@ -0,0 +1,10 @@
+num = input("Enter your number: ")
+num1 = num[0]
+num2 = num[1]
+
+int_num1 = int(num1)
+int_num2 = int(num2)
+int_sum = int_num1+ int_num2
+str_sum =str(int_sum)
+
+print("Sum of this: "+str_sum)

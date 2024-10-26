50278952cd7190c9c9fd8af1dc5ed4210cf763fb
Leshan21 <leshanpasindu12@gmail.com>
2024-10-26 19:24:31 +0530

    other type convert

--- DAY2/lesson2.py
+++ DAY2/lesson2.py
@@ -3,3 +3,9 @@ len_name = len(input("What is your name? "))
 new_len_name = str(len_name)
 
 print("your name has "+new_len_name+" of characters.")
+
+#float()
+#int()
+
+#s = "123"
+#print(type(int(s)))

ff7df8fb0f47818fd68dbb5fb78a421200244ea2
Leshan21 <leshanpasindu12@gmail.com>
2024-10-27 19:08:13 +0530

    if-else-condition

---
+++ DAY3/Exercise1.py
@@ -0,0 +1,1 @@
+num = input("Enter your number")
---
+++ DAY3/lesson1.py
@@ -0,0 +1,8 @@
+print("welcome to the rollercoaster..!!")
+
+height = int(input("Enter your height(cm): "))
+
+if height > 120:
+    print("you can ride")
+else:
+    print("you cant ride")    

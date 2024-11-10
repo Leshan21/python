0df71730c2b0602867f7d491b5070fcb9e518763
Leshan21 <leshanpasindu12@gmail.com>
2024-11-10 21:13:38 +0530

    create random float number and create random float number for between any number multiply that number

--- DAY4/lesson1.py
+++ DAY4/lesson1.py
@@ -1,7 +1,10 @@
 import random
-import create_module
+#import create_module
 
 random_num = random.randint(1, 30)
 
 print(random_num)
-print(create_module.pi)
+#print(create_module.pi)
+random_float = random.random()
+
+print(random_float * 5)

b6cbf60620ffe017f63a7acee50cee5f236cbb8f
Leshan21 <leshanpasindu12@gmail.com>
2024-11-14 22:46:08 +0530

    height avg calculator

--- DAY5/exercise1.py
+++ DAY5/exercise1.py
@@ -1,1 +1,10 @@
-scores = input("Enter the scores ")
+scores = input("Enter the students heights: ")
+
+list = scores.split(" ")
+
+sum = 0
+
+for number in range(0, len(list)):
+   sum = sum + int(list[number]) 
+
+print(sum/len(list))   

348e37e4f56d0300391a523280e3b9d688f0daa0
Leshan21 <leshanpasindu12@gmail.com>
2024-12-04 21:16:50 +0530

    add parameters for function

--- DAY8/lessson1.py
+++ DAY8/lessson1.py
@@ -1,4 +1,13 @@
 def my_function():
     print("hello world")
     
-my_function()    
+#my_function()    
+
+#passing parameters to function
+
+def my_function2(name):
+    print(f"My name is {name}")
+    print(f"What do you do {name}?")
+
+
+my_function2("leshan")    

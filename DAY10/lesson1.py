486967de4cdb352d5b621d1a902e7dde5fe48946
Leshan21 <leshanpasindu12@gmail.com>
2025-03-13 18:37:56 +0530

    docstring

--- DAY10/lesson1.py
+++ DAY10/lesson1.py
@@ -1,6 +1,9 @@
 #Function with output
 
 def format_name(f_name, l_name):
+    # Docstring
+    """Take first name and last name and format it to first letter in
+    in each word to upper case"""
     return f"{f_name.title()} {l_name.title()}"
     
 print(format_name("leshan", "pasindu"))    
